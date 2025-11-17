6a6f962190de1f3e1e60b73de4932b5ae49eeb02
deborahcromer <d.cromer@unsw.edu.au>
2025-11-17 16:20:12 +1100

    fix tags

--- processing/03_retag_covidence_data.R
+++ processing/03_retag_covidence_data.R
@@ -19,6 +19,7 @@ is_nct = function(tags){
   str_detect(tags,"NCT number") & !str_detect(tags,"No NCT number")
 }
 write_csv(data.frame(), "added_tags.csv", append=F)
+write_csv(data.frame("added_tag"), "added_tags.csv", append=T)
 
 add_tags = function(tags){
   #browser()
@@ -55,10 +56,10 @@ add_tags = function(tags){
   # Now add the product type and manufacturer tag
   product_information_tags = c("monoclonal antibody","live-attenuated","protein subunit","viral vector","combination vaccine","mRNA","virus-like particles","protein based particle/subunit vaccine","recombinant vector vaccine","chimeric vaccine","formalin inactivated vaccine","nucleic acid vaccine","formalin inactivated")
   names(product_information_tags) = product_information_tags
-  #retired_tags = c("protein based particle/subunit vaccine","recombinant vector vaccine","chimeric vaccine","nucleic acid vaccine")
-  #names(product_information_tags[product_information_tags %in% retired_tags]) = c("protein subunit", "viral vector", "other", "mRNA")
-  #product_tag_matches = names(product_information_tags)[str_detect(tags, product_information_tags)]
-  product_tag_matches = product_information_tags[str_detect(tags, product_information_tags)]
+  retired_tags = c("protein based particle/subunit vaccine","recombinant vector vaccine","chimeric vaccine","nucleic acid vaccine")
+  names(product_information_tags)[product_information_tags %in% retired_tags] = c("protein subunit", "viral vector", "other", "mRNA")
+  product_tag_matches = names(product_information_tags)[str_detect(tags, product_information_tags)]
+  #product_tag_matches = product_information_tags[str_detect(tags, product_information_tags)]
   product_tag = if (length(product_tag_matches) > 0) product_tag_matches[which.max(nchar(product_tag_matches))] else NA
   
   manufacturer_tags = c("Pfizer","Novavax","GSK","AstraZeneca-Sanofi","Moderna","Janssen","Bavarian Nordic","MedImmune","Sanofi","AstraZeneca","Merck","Sanofi/LID/NIAID/NIH","Sanofi/NIAID/NIH","NIH","NIAID/NIH/MedImmune","NIH/NIAID/VRC","NIAID/NIH","NIAID","Pontificia Universidad Católica de Chile","Virtuvax","Virometix","Blue Lake Biotechnology Inc","Intravacc","Lederle Praxis Biologicals","NIH/Wyeth Vaccines","Praxis Biologics","Wyeth-Lederle Vaccine and Pediatrics","IDT Biologika","Mucosis")
@@ -76,13 +77,18 @@ add_tags = function(tags){
   nct_tag = if(grepl(nct_types[1],tags)) nct_types[1] else if(grepl(nct_types[2],tags)) nct_types[2] else "NA"
   rct_tag = if(grepl(rct_types[1],tags)) rct_types[1] else if(grepl(rct_types[2],tags)) rct_types[2] else if(grepl(rct_types[3],tags)) rct_types[3] else "NA"
   added_tag = glue("{nct_tag}:{rct_tag}")
-  write_csv(data.frame(added_tag), "added_tags.csv", append=T)
-  tags = paste0(tags,"; ",added_tag)
+  
   
   # Now add the approval tag
   if(grepl(nct_types[1],tags)) {
     tags = paste0(tags,"; ","approval unknown") 
   }
+  approval_tags = c("approved","not approved","approval unknown")
+  approval_tag_matches = approval_tags[str_detect(tags, approval_tags)]
+  approval_tag = if (length(approval_tag_matches) > 0) approval_tag_matches[which.max(nchar(approval_tag_matches))] else NA
+  added_tag = glue("{product_tag}:{approval_tag}")
+  write_csv(data.frame(added_tag), "added_tags.csv", append=T)
+  tags = paste0(tags,"; ",added_tag)
   tags
 }
 
